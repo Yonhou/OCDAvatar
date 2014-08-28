e312811113162e8fda6adc150d093f58f219db13
yongchao <i.punk@qq.com>
2014-08-28 21:48:16 +0800

    Initial revision.

---
+++ OCDAvatar/OCDAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OCDAAppDelegate.h
+//  OCDAvatar
+//
+//  Created by Hou Yongchao on 14-8-28.
+//  Copyright (c) 2014年 Hou Yongchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCDAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OCDAvatar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OCDAvatar
+//
+//  Created by Hou Yongchao on 14-8-28.
+//  Copyright (c) 2014年 Hou Yongchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OCDAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OCDAAppDelegate class]));
+    }
+}
